08515185b897f1b5b87a4d0cb7c94493cf734f8c
stefanjocic <stefan.jocic@maksimer.com>
2025-11-29 16:23:25 +0100

    Refactor factories

--- database/factories/InvoiceItemFactory.php
+++ database/factories/InvoiceItemFactory.php
@@ -2,22 +2,29 @@
 
 namespace Database\Factories;
 
+use App\Models\Invoice;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InvoiceItem>
  */
-class InvoiceItemFactory extends Factory
-{
+class InvoiceItemFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
+        $invoice = Invoice::all()->random();
+
         return [
-            //
+            'invoice_id'  => $invoice->id,
+            'description' => $this->faker->sentence( 6 ),
+            'quantity'    => $this->faker->numberBetween( 1, 10 ),
+            'unit_price'  => $this->faker->numberBetween( 20, 100 ),
+            'total'       => function ( array $attributes ) {
+                return $attributes[ 'quantity' ] * $attributes[ 'unit_price' ];
+            },
         ];
     }
 }
--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -2,22 +2,26 @@
 
 namespace Database\Factories;
 
+use App\Models\Invoice;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Payment>
  */
-class PaymentFactory extends Factory
-{
+class PaymentFactory extends Factory {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
+        $invoice = Invoice::all()->random();
+
         return [
-            //
+            'invoice_id'   => $invoice->id,
+            'amount'       => $invoice->total_amount,
+            'payment_date' => $invoice->due_date->addDays( $this->faker->numberBetween( 1, 10 ) ),
+            'method'       => $this->faker->randomElement( [ 'Credit Card', 'Bank Transfer', 'PayPal', 'Cash' ] ),
         ];
     }
 }
