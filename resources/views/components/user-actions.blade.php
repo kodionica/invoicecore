a2881ade03eadc1967fb8d5a449bbfff90d3be1f
Stefan Jocić <38115532+viljuska@users.noreply.github.com>
2026-02-04 21:59:35 +0100

    Localize user action labels in Blade template to Serbian

--- resources/views/components/user-actions.blade.php
+++ resources/views/components/user-actions.blade.php
@@ -4,10 +4,9 @@
             <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                 <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
             </a>
-            <ul class="dropdown-menu text-small" style="">
-                <li><a class="dropdown-item" href="#">New project...</a></li>
-                <li><a class="dropdown-item" href="{{ route('settings.invoice.edit') }}">Settings</a></li>
-                <li><a class="dropdown-item" href="#">Profile</a></li>
+            <ul class="dropdown-menu text-small">
+                <li><a class="dropdown-item" href="{{ route('settings.invoice.edit') }}">Podešavanja firme</a></li>
+                <li><a class="dropdown-item" href="#">Podešavanja profila</a></li>
                 <li>
                     <hr class="dropdown-divider">
                 </li>
@@ -15,7 +14,7 @@
                     <form action="{{ route('logout') }}" method="POST" class="dropdown-item logout-form">
                         @csrf
                         @method('DELETE')
-                        <button class="btn p-0 text-danger logout-button">Log out</button>
+                        <button class="btn p-0 text-danger logout-button">Odjava</button>
                     </form>
                 </li>
             </ul>
@@ -24,8 +23,8 @@
 
     @guest
         <div class="guest-actions space-x-4">
-            <a href="{{ route('login') }}" class="btn btn-outline-primary login-link">Log in</a>
-            <a href="{{ route('register') }}" class="btn signup-button">Sign up</a>
+            <a href="{{ route('login') }}" class="btn btn-outline-primary login-link">Prijava</a>
+            <a href="{{ route('register') }}" class="btn signup-button">Registracija</a>
         </div>
     @endguest
 </div>
