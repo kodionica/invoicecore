304d6b1d9b8f91cfcde5a4eca3c507fad0a8741e
Stefan Jocić <38115532+viljuska@users.noreply.github.com>
2026-02-12 13:35:08 +0100

    Remove company settings views and refactor authentication views to use form components.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,26 +1,13 @@
 <x-layouts.layout>
     <x-page-header heading="Prijava"/>
 
-    <x-forms.form action="{{ route('login.store') }}" method="POST" class="my-5 col-md-8 mx-auto">
-        <div class="form-row">
-            <div class="form-floating">
-                <input type="email" name="email" class="form-control" id="email" required placeholder="Email" autocomplete="email">
-                <label for="email">Email</label>
-            </div>
+    <x-forms.form action="{{ route('login.store') }}" method="POST">
+        <x-forms.input name="email" label="Email" required/>
+        <x-forms.input name="password" label="Šifra" type="password" required/>
+        <x-forms.checkbox name="remember" label="Zapamti me"/>
 
-            <div class="form-floating">
-                <input type="password" name="password" class="form-control" id="password" required placeholder="Šifra" autocomplete="current-password">
-                <label for="password">Šifra</label>
-            </div>
-
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="remember" id="remember">
-                <label class="form-check-label" for="remember">Zapamti me</label>
-            </div>
-
-            <div class="form-group columns-12">
-                <button type="submit" class="btn btn-primary">Prijavi se</button>
-            </div>
+        <div class="form-group columns-12">
+            <button type="submit" class="btn btn-primary">Prijavi se</button>
         </div>
     </x-forms.form>
 </x-layouts.layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,40 +1,20 @@
 <x-layouts.layout>
     <x-page-header heading="Registruj se"/>
 
-    <x-forms.form action="{{ route('register.store') }}" method="POST" class="my-5 col-md-8 mx-auto">
+    <x-forms.form action="{{ route('register.store') }}" method="POST">
+        <x-forms.input name="first_name" label="Ime"/>
+        <x-forms.input name="last_name" label="Prezime"/>
+        <x-forms.input name="email" label="Email" type="email"/>
+        <x-forms.input name="phone" label="Telefon" type="tel"/>
+        <x-forms.input name="password" label="Šifra" type="password" autocomplete="new-password" aria-describedby="passwordHelpBlock">
+            <div id="passwordHelpBlock" class="form-text">
+                Šifra mora biti minimum dužine 5 karaktera
+            </div>
+        </x-forms.input>
+        <x-forms.input name="password_confirmation" label="Potvrdi šifru" type="password"/>
 
-        <div class="form-row">
-            <div class="form-floating ">
-                <input type="text" name="first_name" class="form-control" id="first_name" placeholder="Ime" autocomplete="first name">
-                <label for="first_name">Ime</label>
-            </div>
-            <div class="form-floating ">
-                <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Prezime" autocomplete="last name">
-                <label for="last_name">Prezime</label>
-            </div>
-            <div class="form-floating ">
-                <input type="email" name="email" class="form-control" id="email" required placeholder="Email" autocomplete="email">
-                <label for="email">Email</label>
-            </div>
-            <div class="form-floating ">
-                <input type="tel" name="phone" class="form-control" id="phone" placeholder="Telefon" autocomplete="phone">
-                <label for="phone">Telefon</label>
-            </div>
-            <div class="form-floating ">
-                <input type="password" name="password" class="form-control" id="password" required placeholder="Šifra" autocomplete="new-password" aria-describedby="passwordHelpBlock">
-                <label for="password">Šifra</label>
-                <div id="passwordHelpBlock" class="form-text">
-                    Šifra mora biti minimum dužine 5 karaktera
-                </div>
-            </div>
-            <div class="form-floating ">
-                <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" required placeholder="Potvrdi šifru" autocomplete="new-password">
-                <label for="password_confirmation">Potvrdi šifru</label>
-            </div>
-
-            <div class="form-group columns-12">
-                <button type="submit" class="btn btn-primary">Registruj se</button>
-            </div>
+        <div class="form-group columns-12">
+            <button type="submit" class="btn btn-primary">Registruj se</button>
         </div>
     </x-forms.form>
 </x-layouts.layout>
