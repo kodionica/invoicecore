079ab6fc22c4ab5b440c05529e118aa1e026eaa0
Stefan Jocić <38115532+viljuska@users.noreply.github.com>
2026-02-17 06:48:42 +0100

    Wrap `x-user-active-company` component in `@auth` directive to ensure it only renders for authenticated users.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,7 +3,9 @@
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <x-logo/>
 
-            <x-user-active-company :company="auth()->user()->activeCompany" :user="auth()->user()"/>
+            @auth
+                <x-user-active-company :company="auth()->user()->activeCompany" :user="auth()->user()"/>
+            @endauth
 
             <x-user-actions :user="auth()->user()"/>
         </div>
